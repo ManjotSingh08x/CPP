5dfdefd6f5746f43465cb4667d7fc34bdfc2466e
Manjot Singh <mithugunnu2006@gmail.com>
2024-07-09 23:56:42 -0700

    learning header files

--- LearnCPP/chapter2/lesson2.8/main.cpp
+++ LearnCPP/chapter2/lesson2.8/main.cpp
@@ -1,8 +1,7 @@
+#include "add.h"
 #include <iostream>
 
 //use the direct compiler commands to execute mulitple files
-
-int add(int a, int b);
 int main()
 {
     std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; // compile error
