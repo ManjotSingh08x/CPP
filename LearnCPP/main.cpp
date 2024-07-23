9bb52efa1a7a0467d6504bce4153bab6b646b582
Manjot Singh <mithugunnu2006@gmail.com>
2024-07-23 03:24:15 -0700

    learnng about data types

--- LearnCPP/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "add.h"
-#include <iostream>
-
-//use the direct compiler commands to execute mulitple files
-int main()
-{
-    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; // compile error
-    return 0;
-}
